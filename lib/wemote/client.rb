e79550e01e16ae45b81b74b9c0d4e900b7a6b652
Kevin Gisi <kevin@kevingisi.com>
2014-03-07 03:24:18 -0500

    DRY'd up client logic

--- lib/wemote/client.rb
+++ lib/wemote/client.rb
@@ -6,6 +6,7 @@ module Wemote
         constants.collect {|const_name| const_get(const_name)}.select {|const| const.class == Module}.detect do |mod|
           fulfilled = false
           begin
+            next unless mod.const_defined?(:DEPENDENCIES)
             mod.const_get(:DEPENDENCIES).map{|d|require d}
             fulfilled = true
           rescue LoadError
@@ -15,63 +16,57 @@ module Wemote
       end
     end
 
+    module SmartLib
+      %w{get post put}.each do |name|
+        define_method name do |*args|
+          _req(@lib,name,*args).tap{|r|r.call if @call}
+        end
+      end
+    end
+
     module Manticore
       DEPENDENCIES = ['manticore']
-
-      def get(*args)
-        _get(::Manticore,*args).call
+      def self.extended(base)
+        base.instance_variable_set(:@lib,::Manticore)
+        base.instance_variable_set(:@call,true)
+        base.extend(SmartLib)
       end
-
-      def post(*args)
-        _post(::Manticore,*args).call
-      end
-
     end
 
     module Typhoeus
       DEPENDENCIES = ['typhoeus']
-
-      def get(*args)
-        _get(::Typhoeus,*args)
+      def self.extended(base)
+        base.instance_variable_set(:@lib,::Typhoeus)
+        base.extend(SmartLib)
       end
-
-      def post(*args)
-        _post(::Typhoeus,*args)
-      end
-
     end
 
     module HTTParty
       DEPENDENCIES = ['httparty']
-
-      def get(*args)
-        _get(::HTTParty,*args)
-      end
-
-      def post(*args)
-        _post(::HTTParty,*args)
+      def self.extended(base)
+        base.instance_variable_set(:@lib,::HTTParty)
+        base.extend(SmartLib)
       end
     end
 
     module NetHTTP
       DEPENDENCIES = ['net/http','uri']
 
-      def get(url,body=nil,headers=nil)
+      def request(klass,url,body=nil,headers=nil)
         uri = URI.parse(url)
         http = Net::HTTP.new(uri.host, uri.port)
-        request = Net::HTTP::Get.new(uri.request_uri)
-        headers.map{|k,v|request[k]=v} if headers
-        response = http.request(request)
-      end
-
-      def post(url,body=nil,headers=nil)
-        uri = URI.parse(url)
-        http = Net::HTTP.new(uri.host, uri.port)
-        request = Net::HTTP::Post.new(uri.request_uri)
+        request = klass.new(uri.request_uri)
         headers.map{|k,v|request[k]=v} if headers
         (request.body = body) if body
         response = http.request(request)
       end
+
+      %w{get post put}.each do |name|
+        define_method name do |*args|
+          request(Net::HTTP.const_get(name.capitalize),*args)
+        end
+      end
+
     end
 
     def initialize
@@ -80,12 +75,8 @@ module Wemote
 
     private
 
-    def _get(lib,url,body=nil,headers=nil)
-      lib.get(url,{body:body,headers:headers})
-    end
-
-    def _post(lib,url,body=nil,headers=nil)
-      lib.post(url,{body:body,headers:headers})
+    def _req(lib,method,url,body=nil,headers=nil)
+      lib.send(method,url,{body:body,headers:headers})
     end
 
   end
